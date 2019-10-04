fa6246eae2157ac4b99998c0ae1a5f7d582adefd
조영재 <wo78644@gmail.com>
2019-10-05 03:58:46 +0900

    update infogan

--- API/server_api/views.py
+++ API/server_api/views.py
@@ -9,15 +9,14 @@ from .serializers import CalliSerializer
 from .models import Post
 from .models import Calli
 from rest_framework import permissions
-from .keras_load_model import excute
-from .keras_load_model import ready
+from .main import main
+from .main import ready
 import tensorflow as tf
 
-
-model_list, z_list, alphabet = ready()
+global graph
 graph = tf.get_default_graph()
+font_list, model_list, z_list = ready()
 
-model_list[0].summary()
 
 class PostView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -36,16 +35,15 @@ class CalliView(viewsets.ModelViewSet):
         serializer_class = CalliSerializer
 
         def perform_create(self, serializer):
-            name = self.request.data['name']
-            param1 = self.request.data['param1']
-            param2 = self.request.data['param2']
-            param3 = self.request.data['param3']
-            param4 = self.request.data['param4']
-            param5 = self.request.data['param5']
-
-            excute(model_list, z_list, alphabet, name, int(param1), int(param2), int(param3), int(param4),
-                   int(param5))
-            serializer.save()
+            input_text = self.request.data['input_text']
+            font = int(self.request.data['font'])
+            latter_attach = int(self.request.data['latter_attach'])
+            definition = int(self.request.data['definition'])
+            color = int(self.request.data['color'])
+            
+            with graph.as_default():
+                main(font_list, model_list, z_list, font, input_text, latter_attach, definition, color)
+            # serializer.save()
 
 
 def index(request):
